55c4b63f531ef2c7a079cfa7011c0cd0bb9b50a1
PasaDzumhur <pdzumhur1@etf.unsa.ba>
2021-01-01 23:06:23 +0100

    get predmete

--- local.js
+++ local.js
@@ -2,15 +2,46 @@ const express = require("express");
 const session = require("express-session");
 const http = require('http');
 const url = require('url');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
-
+console.log("testPrije");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+"/public"));
-app.get('/',function (req,res){
-    var param=url.parse(req.url,true).query.q;
-    res.sendFile(__dirname+"/public/" + param);
+app.get('/predmeti',function (req,res){
+    //console.dir("test");
+    let param=url.parse(req.url,true).query;
+
+    fs.readFile("predmeti.txt",function read (err,buf){
+
+        if(err) console.log(err);
+        //res.writeHead(200, {'Content-Type': 'application/json'});
+        let text = buf.toString();
+        //console.log("text");
+        let textovi = text.split('\n');
+        //
+
+        let stringJson = "[";
+        for( let i = 1; i<textovi.length; i++){
+            if(i!=1) stringJson+=",";
+            stringJson += "{'"+textovi[0]+"':'"+textovi[i]+"'}";
+        }
+        stringJson+="]";
+        res.json(stringJson);
+
+        //res.json({textovi});
+    });
+
+    //res.send("radi");
+
+
 
 });
+
+
+
 
 
 
