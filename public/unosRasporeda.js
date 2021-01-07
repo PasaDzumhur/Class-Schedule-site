59a6fd2f1ae8b5787b25a0b0d57809e6843d9ed2
PasaDzumhur <pdzumhur1@etf.unsa.ba>
2021-01-07 18:51:56 +0100

    4. zadatak kupi podatke iz txt fajlova

--- public/unosRasporeda.js
+++ public/unosRasporeda.js
@@ -2,75 +2,43 @@
 
 
 var predmeti,aktivnosti;
-var naziv,tip,pocetak,kraj,dan;
-window.onload = function (){
-    /*
-    fs.readFile("predmeti.txt",function read ( err, buf){
-        if(err) throw err;
-        predmeti = buf.toString().split('\n');
-    })
-    fs.readFile("aktivnosti.txt", function read(err, buf){
-        if(err) throw err;
-        aktivnosti = buf.toString().split('\n');
-    })*/
-    predmeti = dajPredmete(ispisi());
-    aktivnosti = dajAktivnosti(ispisi());
-    naziv = document.getElementById("naziv");
-    tip = document.getElementById("tip");
-    pocetak = document.getElementById("pocetak");
-    kraj = document.getElementById("kraj");
-    dan = document.getElementById("dan");
-    document.getElementById("unesi").addEventListener("click",function (){unesi(naziv,tip,pocetak,kraj,dan)});
-}
-function ispisi(sadzraj){
-    console.log(sadzraj);
-}
-function dajPredmete(fnCallback){
+function dajPredmete(){
     var ajax = new XMLHttpRequest();
 
     ajax.onreadystatechange = function() {// Anonimna funkcija
         if (ajax.readyState == 4 && ajax.status == 200){
-            predmeti = ajax.responseText;
-            fnCallback(predmeti);
-
+            predmeti = JSON.parse(this.responseText);
         }
-        else if (ajax.readyState == 4){
-            fnCallback("prazno");
-        }
-
     }
+    ajax.open("GET",'/predmet',true);
+    ajax.send();
 
 
-    ajax.open("GET","http://localhost:3000/predmet",true);
-    ajax.setRequestHeader("Content-Type", "application/json");
 
 }
 function dajAktivnosti(){
-    var ajax = new XMLHttpRequest();
+    var ajax2 = new XMLHttpRequest();
 
-    ajax.onreadystatechange = function() {// Anonimna funkcija
-        if (ajax.readyState == 4 && ajax.status == 200){
-            predmeti = ajax.responseText;
-            fnCallback(aktivnosti);
-
-        }
-        else if (ajax.readyState == 4){
-            fnCallback("prazno");
+    ajax2.onreadystatechange = function() {// Anonimna funkcija
+        if (ajax2.readyState == 4 && ajax2.status == 200){
+            aktivnosti = JSON.parse(ajax2.responseText);
         }
 
     }
 
-    ajax.open("GET","http://localhost:3000/aktivnosti",true);
-    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax2.open("GET",'/aktivnosti',true);
+    ajax2.send();
 
 
 }
 
-function unesi(naziv,tip,pocetak,kraj,dan){
-    console.log("naziv: " + naziv.innerHTML);
-    console.log("tip: " + tip.innerHTML);
-    console.log("pocetak: " + pocetak.innerHTML);
-    console.log("kraj: " + kraj.innerHTML);
-    console.log("dan: " + dan.innerHTML);
+function unesi(){
+    alert(predmeti);
+    alert(aktivnosti)
 
+
+}
+window.onload = function (){
+    dajPredmete();
+    dajAktivnosti();
 }
